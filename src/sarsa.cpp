dee576b8f45ee1fa241261969d3acd4446c960f8
juliocesar-ramirez <julio.ramirez@pucp.pe>
2024-04-05 11:41:15 -0500

    sarsaAlgorthim update
    ahora lee punteros, y es un static

--- src/sarsa.cpp
+++ src/sarsa.cpp
@@ -1,4 +1,5 @@
 #include "sarsa.h"
+#include "iostream"
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // static member
@@ -11,43 +12,40 @@ const double sarsa::gamma = 0.9;
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // constructor
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sarsa::sarsa() {
-    setQCurrent(0);
-    setQPrevious(0);
-    setR(0);
+sarsa::sarsa() : QPrevious(nullptr), QCurrent(nullptr), r(nullptr) {
 }
-sarsa::sarsa(double QCurrent, double QPrevious, double r){
-    setQCurrent(QCurrent);
-    setQPrevious(QPrevious);
-    setR(r);
-}
+// sarsa::sarsa(double QCurrent, double QPrevious, double r){
+//     setQCurrent(QCurrent);
+//     setQPrevious(QPrevious);
+//     setR(r);
+// }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // setters
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void sarsa::setQCurrent(double QCurrent) {
+void sarsa::setQCurrent(double* QCurrent) {
     (*this).QCurrent = QCurrent;
 }
-void sarsa::setQPrevious(double QPrevious) {
+void sarsa::setQPrevious(double* QPrevious) {
     (*this).QPrevious = QPrevious;
 }
-void sarsa::setR(int r) {
+void sarsa::setR(int* r) {
     (*this).r = r;
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // getters
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-double sarsa::getQCurrent() {
+double* sarsa::getQCurrent() {
     return QCurrent;
 }
-double sarsa::getQPrevious() {
+double* sarsa::getQPrevious() {
     return QPrevious;
 }
-double sarsa::getR() {
+int* sarsa::getR() {
     return r;  
 }
 
 void sarsa::sarsaActualizarQ() {
-    QPrevious += alpha * (r + gamma * QCurrent - QPrevious); 
+    *QPrevious += alpha * (*r + gamma * *QCurrent - *QPrevious); 
 }
