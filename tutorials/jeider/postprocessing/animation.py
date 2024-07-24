b899e3ca5981ae70c65fb04e4f8fdd11556d61c7
juliocesar-ramirez <julio.ramirez@pucp.pe>
2024-07-24 11:16:06 -0500

    animation update
    utiliza set limites para forzar visualizacion

--- tutorials/jeider/postprocessing/animation.py
+++ tutorials/jeider/postprocessing/animation.py
@@ -4,14 +4,22 @@ import matplotlib.pyplot as plt
 import os
 from matplotlib.animation import FuncAnimation
 from screeninfo import get_monitors
+from progressbar import ProgressBar
+from os import path
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes as zoom_inset_axes
+#files
+ruta_absoluta = path.abspath(__file__)
+directory_main = path.dirname(path.dirname(ruta_absoluta))
+directory_mesh = path.join(directory_main, "mesh/poliLinks")
+directory_data = path.join(directory_main, "data/")
+directory_export = path.join(directory_main, "postprocessing/snapshot/")
 
-pathMeshLink = "../mesh/poliLinks"
 x1_values = []
 x2_values = []
 y1_values = []
 y2_values = []
 
-with open(pathMeshLink, 'r') as csv_file:
+with open(directory_mesh, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=' ')
 
     for row in csv_reader:
@@ -20,15 +28,16 @@ with open(pathMeshLink, 'r') as csv_file:
         x2_values.append(float(row[2]))
         y2_values.append(float(row[3]))
 
-directorio_principal = '../'
-elementos = os.listdir(directorio_principal)
+elementos = os.listdir(directory_data)
 carpetas_numericas = [elemento
                       for elemento
                       in elementos
-                      if os.path.isdir(os.path.join(directorio_principal,
+                      if os.path.isdir(os.path.join(directory_data,
                                                     elemento))
                       and elemento.isdigit()]
 carpetas_numericas_ordenadas = sorted(carpetas_numericas, key=int)
+bar = ProgressBar(maxval=len(carpetas_numericas_ordenadas)).start()
+
 width = 12
 height = 5
 # tamaño de figura
@@ -37,18 +46,16 @@ screen_height = get_monitors()[0].height
 width = screen_width/100
 height = screen_height/100
 
-fig, ax = plt.subplots(1, 1, figsize=(width, height))
+fig, ax = plt.subplots(1, 1, figsize=(width, height), tight_layout=True)
 i_inicial = int(carpetas_numericas_ordenadas[0])
 
 
 def actualizar(i):
     """Loop each timestep."""
     ax.clear()
-    # i = i_inicial + i
-    print(carpetas_numericas_ordenadas[i])
-    fileName = directorio_principal + carpetas_numericas_ordenadas[i] + "/xy"
-    fileName2 = directorio_principal + carpetas_numericas_ordenadas[i] + "/U"
-    fileName3 = directorio_principal + carpetas_numericas_ordenadas[i] + "/cantPedestrianEvacuated"
+    fileName = directory_data + carpetas_numericas_ordenadas[i] + "/xy"
+    fileName2 = directory_data + carpetas_numericas_ordenadas[i] + "/U"
+    fileName3 = directory_data + carpetas_numericas_ordenadas[i] + "/cantPedestrianEvacuated"
     x_values = []
     y_values = []
     magnitud = []
@@ -77,16 +84,27 @@ def actualizar(i):
     scatter = ax.scatter(x_values, y_values, c=magnitud,
                          cmap="jet_r", marker='o', edgecolors="none",
                          vmin=vmin, vmax=vmax)
+
+    axins = zoom_inset_axes(ax, zoom=5, loc='upper right')
+    axins.plot([x1_values, x2_values], [y1_values, y2_values], c="k", lw=1)
+    axins.scatter(x_values, y_values, c=magnitud,
+                  cmap="jet_r", marker='o', edgecolors="none",
+                  vmin=vmin, vmax=vmax)
+    axins.set_xlim({746500, 747500})
+    axins.set_ylim({8157000, 8157500})
+
     # texto
     cantPeEv = str(int(cantPedestrianEvacuated[0]))
     text1 = "t = " + str(i) + " seg; evacuated: " + cantPeEv
-    ax.text(0.05, 0.03, text1, fontsize=12, fontweight='normal',
+    ax.text(0.05, 0.02, text1, fontsize=20, fontweight='normal',
             transform=ax.transAxes)
-
-    ax.xaxis.set_label_coords(0, -0.06)
+    # configuraciones
+    ax.axis('off')
+    # ax.xaxis.set_label_coords(0, -0.06)
+    # ax.set_axis_off()
     if i == i_inicial:
-        cax = fig.add_axes([0.93, 0.1, 0.02, 0.8])
-        plt.colorbar(scatter, cax=cax)
+        # cax = fig.add_axes([0.93, 0.1, 0.02, 0.8])
+        plt.colorbar(scatter, ax=ax, fraction=0.03)
     # ax.set_axis_off()
     # if (i == "1"):
     #     xlim_auto = ax.get_xlim()
@@ -94,8 +112,12 @@ def actualizar(i):
     # else:
     #     ax.set_xlim(xlim_auto)
     #     ax.set_ylim(ylim_auto)
+    bar.update(i + 1)
 
+    ax.set_xlim([743000, 756500])
+    ax.set_ylim([8155500, 8161000])
 
 ani = FuncAnimation(fig, actualizar, len(carpetas_numericas_ordenadas)-1,
-                    interval=400, repeat_delay=900)
-ani.save('animation.mp4')
+                    interval=100, repeat_delay=900)
+ani.save(directory_export + 'animation.mp4')
+bar.finish()
